50ed1c84176f268bfc93107f1561e396fb6b71e9
crazypriyanshu <priyanshu@reputation.com>
2023-10-07 01:31:48 +0530

    This is my first commit for learning spring boot inheritance types and creating db, 1 TC also written to add data

---
+++ src/main/java/dev/priyanshu/playgroundproject/PlayGroundProjectApplication.java
@@ -0,0 +1,13 @@
+package dev.priyanshu.playgroundproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayGroundProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayGroundProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/priyanshu/playgroundproject/PlayGroundProjectApplicationTests.java
@@ -0,0 +1,42 @@
+package dev.priyanshu.playgroundproject;
+
+import dev.priyanshu.playgroundproject.InheritanceExamples.JoinedTable.Mentor;
+import dev.priyanshu.playgroundproject.InheritanceExamples.JoinedTable.MentorRepository;
+import dev.priyanshu.playgroundproject.InheritanceExamples.JoinedTable.User;
+import dev.priyanshu.playgroundproject.InheritanceExamples.JoinedTable.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+class PlayGroundProjectApplicationTests {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private MentorRepository mentorRepository;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testDiffInheritance() {
+        User user = new User();
+        user.setEmail("priyanshu@test.com");
+        user.setPassword("pass");
+        userRepository.save(user);
+
+        Mentor mentor = new Mentor();
+        mentor.setNoOfSessions(43);
+        mentor.setPassword("passsword");
+        mentor.setEmail("mentor@ew.com");
+        mentor.setId(2);
+        mentorRepository.save(mentor);
+        Optional<Mentor> m = mentorRepository.findMentorByIdIs(new Long(2));
+        System.out.println("mentor is : "+m.getClass().getName());
+
+    }
+
+}
